b167f3308dbff0191ef6bde922a6ce2b44e52049
Oliver Rudzick <oliver@rudzick.de>
2026-04-11 17:19:47 +0200

    Added views for displaying both ATMs mapped as amenity and as a part of polygons (e.g. buildings)

---
+++ Server_Update/views_for_atm.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE VIEW fake_geldautomaten AS
+SELECT ap.piageom AS geom, ap.tags AS tags
+FROM atm_polygons AS ap
+LEFT OUTER JOIN atm AS a
+ON ST_INTERSECTS(ap.geom, a.geom) AND ap.geom && a.geom
+WHERE a.geom IS NULL;
+
+CREATE OR REPLACE VIEW alle_geldautomaten AS
+SELECT geom, tags FROM fake_geldautomaten
+UNION ALL
+SELECT geom, tags FROM atm;
