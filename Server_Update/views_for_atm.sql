81796ea7a37d12a77b398b72518d2830459ebc3b
rudzick <oliver@rudzick.de>
2026-04-13 09:57:44 +0200

    added table cash_withdrawal in theme atm

--- Server_Update/views_for_atm.sql
+++ Server_Update/views_for_atm.sql
@@ -9,3 +9,15 @@ CREATE OR REPLACE VIEW alle_geldautomaten AS
 SELECT geom, tags FROM fake_geldautomaten
 UNION ALL
 SELECT geom, tags FROM atm;
+
+CREATE OR REPLACE VIEW fake_cash_withdrawal AS
+SELECT ap.piageom AS geom, ap.tags AS tags
+FROM cash_withdrawal_polygons AS ap
+LEFT OUTER JOIN cash_withdrawal AS a
+ON ST_INTERSECTS(ap.geom, a.geom) AND ap.geom && a.geom
+WHERE a.geom IS NULL;
+
+CREATE OR REPLACE VIEW alle_cash_withdrawal AS
+SELECT geom, tags FROM fake_cash_withdrawal
+UNION ALL
+SELECT geom, tags FROM cash_withdrawal;
--- themes/shortbread_v1/topics/atm.lua
+++ themes/shortbread_v1/topics/atm.lua
@@ -1,7 +1,7 @@
 -- ---------------------------------------------------------------------------
 --
 -- Theme: shortbread_v1
--- Topic: allotment_garden_name
+-- Topic: ATM
 --
 -- ---------------------------------------------------------------------------
 
@@ -22,6 +22,32 @@ themepark:add_table{
 
 themepark:add_table{
     name = 'atm_polygons',
+    ids_type = 'way',
+    geom = 'polygon',
+    columns = themepark:columns({
+        { column = 'tags', type = 'jsonb' },
+        { column = 'piageom', type = 'point' },
+    }),
+    tags = { },
+    tiles = {
+    minzoom = 9,
+	},
+}
+themepark:add_table{
+    name = 'cash_withdrawal',
+    ids_type = 'any',
+    geom = 'point',
+    columns = themepark:columns({
+        { column = 'tags', type = 'jsonb' },
+    }),
+    tags = { },
+    tiles = {
+        minzoom = 9,
+    },
+}
+
+themepark:add_table{
+    name = 'cash_withdrawal_polygons',
     ids_type = 'way',
     geom = 'polygon',
     columns = themepark:columns({
@@ -67,6 +93,46 @@ themepark:add_proc('way', function(object, data)
     
     if a then
          themepark:insert('atm_polygons', {
+	 	geom = a_geom,
+		tags= object.tags,
+		piageom = pia_geom
+		}
+		)
+    end
+
+end)
+
+themepark:add_proc('node', function(object, data)
+
+    if ( object.tags.cash_withdrawal and not ( object.tags.cash_withdrawal == 'no' or object.tags.cash_withdrawal == 'unknown' ) then
+
+       local a = object
+       local cash_withdrawal_geom =  object:as_point()
+
+       themepark:insert('cash_withdrawal', {
+		geom = cash_withdrawal_geom,
+		tags = object.tags,
+		     }
+		)    
+    end		
+end)
+
+themepark:add_proc('way', function(object, data)
+
+    if not object.is_closed or not object.tags.cash_withdrawal  then
+        return
+    end
+
+    if object.tags.cash_withdrawal == 'no' or object.tags.cash_withdrawal == 'unknown' then
+       return
+    end
+
+    local a = object
+    local a_geom = object:as_polygon()
+    local pia_geom = object:as_polygon():pole_of_inaccessibility()
+    
+    if a then
+         themepark:insert('cash_withdrawal_polygons', {
 	 	geom = a_geom,
 		tags= object.tags,
 		piageom = pia_geom
