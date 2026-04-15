ba74d045dfee3e5b162172386a07e597dec4acca
rudzick <oliver@rudzick.de>
2026-04-15 03:47:39 +0200

    Added shops with known cash withdrawal possibility in ATM topic to table chash_withdrawal

--- themes/shortbread_v1/topics/atm.lua
+++ themes/shortbread_v1/topics/atm.lua
@@ -113,8 +113,380 @@ themepark:add_proc('node', function(object, data)
 		geom = cash_withdrawal_geom,
 		tags = object.tags,
 		     }
-		)    
+		)
     end		
+end)
+
+themepark:add_proc('node', function(object, data)
+-- https://www.hanseaticbank.de/klarmacher/bezahlen/geld-abheben-im-supermarkt-so-funktioniert-cashback-bei-rewe-lidl-co#so
+	if ( object.tags.cash_withdrawal or not object.tags.shop or object.tags.shop == 'no' or object.tags.shop == 'unknown' ) then
+	   return
+	end
+
+	local a = object
+	if object.tags.brand then
+	   local brand = string.lower(object.tags.brand)
+	else
+	   local brand = ''
+	end
+	if object.tags['brand:wikidata'] then
+	   local brandwikidata = string.lower(object.tags['brand:wikidata'])
+	else
+	   local brandwikidata = ''
+	end
+	if object.tags.operator then
+	   local operator = string.lower(object.tags.operator)
+	else
+	   local operator = ''
+	end
+	if object.tags['operator:wikidata'] then
+	   local operatorwikidata = string.lower(object.tags['operator:wikidata'])
+	else
+	   local operatorwikidata = ''
+	end
+	local cash_withdrawal_geom =  object:as_point()
+	
+	-- EDEKA
+	local company = 'edeka'
+	local companywikidata = 'q701755'
+	if ( brand == company or operator == company or brandwikidata == companywikidata or operatorwikidata == companywikidata ) then
+
+	   object.tags.cash_withdrawal = 'implicit'
+	   object.tags['cash_withdrawal:purchase_required'] = 'yes'
+	   object.tags['cash_withdrawal:purchase_minimum'] = '20'
+	   object.tags['cash_withdrawal:limit'] = '200'
+	   
+	   themepark:insert('cash_withdrawal', {
+			       geom = cash_withdrawal_geom,
+			       tags = object.tags,
+					       }
+	   )
+
+	   return
+	   
+	end
+	
+	-- REWE
+	local company = 'rewe'
+	local companywikidata = 'q16968817'
+	if ( brand == company or operator == company or brandwikidata == companywikidata or operatorwikidata == companywikidata ) then
+
+	   object.tags.cash_withdrawal = 'implicit'
+	   object.tags['cash_withdrawal:purchase_required'] = 'yes'
+	   object.tags['cash_withdrawal:purchase_minimum'] = 'no'
+	   object.tags['cash_withdrawal:limit'] = '200'
+	   
+	   themepark:insert('cash_withdrawal', {
+			       geom = cash_withdrawal_geom,
+			       tags = object.tags,
+					       }
+	   )
+
+	   return
+	   
+	end
+	
+	-- ALDI Nord
+	local company = 'aldi nord'
+	local companywikidata = 'q41171373'
+	if ( brand == company or operator == company or brandwikidata == companywikidata or operatorwikidata == companywikidata ) then
+
+	   object.tags.cash_withdrawal = 'implicit'
+	   object.tags['cash_withdrawal:purchase_required'] = 'yes'
+	   object.tags['cash_withdrawal:purchase_minimum'] = '1'
+	   object.tags['cash_withdrawal:limit'] = '200'
+	   
+	   themepark:insert('cash_withdrawal', {
+			       geom = cash_withdrawal_geom,
+			       tags = object.tags,
+					       }
+	   )
+
+	   return
+	   
+	end
+	
+	-- ALDI Süd
+	local company = 'aldi süd'
+	local companywikidata = 'q41171672'
+	if ( brand == company or operator == company or brandwikidata == companywikidata or operatorwikidata == companywikidata ) then
+
+	   object.tags.cash_withdrawal = 'implicit'
+	   object.tags['cash_withdrawal:purchase_required'] = 'yes'
+	   object.tags['cash_withdrawal:purchase_minimum'] = '5'
+	   object.tags['cash_withdrawal:limit'] = '200'
+	   
+	   themepark:insert('cash_withdrawal', {
+			       geom = cash_withdrawal_geom,
+			       tags = object.tags,
+					       }
+	   )
+
+	   return
+	   
+	end
+	
+	-- ALDI
+	local company = 'aldi'
+	local companywikidata = 'q125054'
+	if ( brand == company or operator == company or brandwikidata == companywikidata or operatorwikidata == companywikidata ) then
+
+	   object.tags.cash_withdrawal = 'implicit'
+	   object.tags['cash_withdrawal:purchase_required'] = 'yes'
+	   object.tags['cash_withdrawal:limit'] = '200'
+	   
+	   themepark:insert('cash_withdrawal', {
+			       geom = cash_withdrawal_geom,
+			       tags = object.tags,
+					       }
+	   )
+
+	   return
+	   
+	end
+	
+	-- DM
+	local company = 'dm'
+	local companywikidata = 'q41171672'
+	if ( brand == company or operator == company or brandwikidata == companywikidata or operatorwikidata == companywikidata ) then
+
+	   object.tags.cash_withdrawal = 'implicit'
+	   object.tags['cash_withdrawal:purchase_required'] = 'yes'
+	   object.tags['cash_withdrawal:purchase_minimum'] = 'no'
+	   object.tags['cash_withdrawal:limit'] = '200'
+	   
+	   themepark:insert('cash_withdrawal', {
+			       geom = cash_withdrawal_geom,
+			       tags = object.tags,
+					       }
+	   )
+
+	   return
+	   
+	end
+	
+	-- Netto
+	local company = 'netto marken-discount'
+	local companywikidata = 'q879858'
+	if ( brand == company or operator == company or brandwikidata == companywikidata or operatorwikidata == companywikidata or brand == 'netto' or operator == 'netto' ) then
+
+	   object.tags.cash_withdrawal = 'implicit'
+	   object.tags['cash_withdrawal:purchase_required'] = 'yes'
+	   object.tags['cash_withdrawal:purchase_minimum'] = '10'
+	   object.tags['cash_withdrawal:limit'] = '200'
+	   
+	   themepark:insert('cash_withdrawal', {
+			       geom = cash_withdrawal_geom,
+			       tags = object.tags,
+					       }
+	   )
+
+	   return
+	   
+	end
+	
+	-- Denns
+	local company = 'denns biomarkt'
+	local companywikidata = 'q48883773'
+	if ( brand == company or operator == company or brandwikidata == companywikidata or operatorwikidata == companywikidata or brand == 'denns' or operator == 'denns' ) then
+
+	   object.tags.cash_withdrawal = 'implicit'
+	   object.tags['cash_withdrawal:purchase_required'] = 'yes'
+	   object.tags['cash_withdrawal:purchase_minimum'] = '20'
+	   object.tags['cash_withdrawal:limit'] = '200'
+	   
+	   themepark:insert('cash_withdrawal', {
+			       geom = cash_withdrawal_geom,
+			       tags = object.tags,
+					       }
+	   )
+
+	   return
+	   
+	end
+	
+	-- famila
+	local company = 'famila'
+	local companywikidata = 'q1395108'
+	if ( brand == company or operator == company or brandwikidata == companywikidata or operatorwikidata == companywikidata ) then
+
+	   object.tags.cash_withdrawal = 'implicit'
+	   object.tags['cash_withdrawal:purchase_required'] = 'yes'
+	   object.tags['cash_withdrawal:purchase_minimum'] = '10'
+	   object.tags['cash_withdrawal:limit'] = '200'
+	   
+	   themepark:insert('cash_withdrawal', {
+			       geom = cash_withdrawal_geom,
+			       tags = object.tags,
+					       }
+	   )
+
+	   return
+	   
+	end
+	
+	-- Kaufland
+	local company = 'kaufland'
+	local companywikidata = 'q685967'
+	if ( brand == company or operator == company or brandwikidata == companywikidata or operatorwikidata == companywikidata ) then
+
+	   object.tags.cash_withdrawal = 'implicit'
+	   object.tags['cash_withdrawal:purchase_required'] = 'yes'
+	   object.tags['cash_withdrawal:purchase_minimum'] = '10'
+	   object.tags['cash_withdrawal:limit'] = '200'
+	   
+	   themepark:insert('cash_withdrawal', {
+			       geom = cash_withdrawal_geom,
+			       tags = object.tags,
+					       }
+	   )
+
+	   return
+	   
+	end
+	
+	-- Lidl
+	local company = 'lidl'
+	local companywikidata = 'q151954'
+	if ( brand == company or operator == company or brandwikidata == companywikidata or operatorwikidata == companywikidata ) then
+
+	   object.tags.cash_withdrawal = 'implicit'
+	   object.tags['cash_withdrawal:purchase_required'] = 'yes'
+	   object.tags['cash_withdrawal:purchase_minimum'] = '0.99'
+	   object.tags['cash_withdrawal:limit'] = '200'
+	   
+	   themepark:insert('cash_withdrawal', {
+			       geom = cash_withdrawal_geom,
+			       tags = object.tags,
+					       }
+	   )
+
+	   return
+	   
+	end
+	
+	-- Markant
+	local company = 'markant'
+	local companywikidata = 'q57523365'
+	if ( brand == company or operator == company or brandwikidata == companywikidata or operatorwikidata == companywikidata ) then
+
+	   object.tags.cash_withdrawal = 'implicit'
+	   object.tags['cash_withdrawal:purchase_required'] = 'yes'
+	   object.tags['cash_withdrawal:purchase_minimum'] = '10'
+	   object.tags['cash_withdrawal:limit'] = '200'
+	   
+	   themepark:insert('cash_withdrawal', {
+			       geom = cash_withdrawal_geom,
+			       tags = object.tags,
+					       }
+	   )
+
+	   return
+	   
+	end
+	
+	-- Norma
+	local company = 'norma'
+	local companywikidata = 'q450180'
+	if ( brand == company or operator == company or brandwikidata == companywikidata or operatorwikidata == companywikidata ) then
+
+	   object.tags.cash_withdrawal = 'implicit'
+	   object.tags['cash_withdrawal:purchase_required'] = 'yes'
+	   object.tags['cash_withdrawal:purchase_minimum'] = '1'
+	   object.tags['cash_withdrawal:limit'] = '200'
+	   
+	   themepark:insert('cash_withdrawal', {
+			       geom = cash_withdrawal_geom,
+			       tags = object.tags,
+					       }
+	   )
+
+	   return
+	   
+	end
+	
+	-- NP-Markt
+	local company = 'np-markt'
+	local companywikidata = 'q15836148'
+	if ( brand == company or operator == company or brandwikidata == companywikidata or operatorwikidata == companywikidata ) then
+
+	   object.tags.cash_withdrawal = 'implicit'
+	   object.tags['cash_withdrawal:purchase_required'] = 'yes'
+	   object.tags['cash_withdrawal:purchase_minimum'] = '10'
+	   object.tags['cash_withdrawal:limit'] = '200'
+	   
+	   themepark:insert('cash_withdrawal', {
+			       geom = cash_withdrawal_geom,
+			       tags = object.tags,
+					       }
+	   )
+
+	   return
+	   
+	end
+	
+	-- Penny
+	local company = 'penny'
+	local companywikidata = 'q284688'
+	if ( brand == company or operator == company or brandwikidata == companywikidata or operatorwikidata == companywikidata ) then
+
+	   object.tags.cash_withdrawal = 'implicit'
+	   object.tags['cash_withdrawal:purchase_required'] = 'yes'
+	   object.tags['cash_withdrawal:purchase_minimum'] = 'no'
+	   object.tags['cash_withdrawal:limit'] = '200'
+	   
+	   themepark:insert('cash_withdrawal', {
+			       geom = cash_withdrawal_geom,
+			       tags = object.tags,
+					       }
+	   )
+
+	   return
+	   
+	end
+	
+	-- Tegut
+	local company = 'tegut'
+	local companywikidata = 'q1547993'
+	if ( brand == company or operator == company or brandwikidata == companywikidata or operatorwikidata == companywikidata ) then
+
+	   object.tags.cash_withdrawal = 'implicit'
+	   object.tags['cash_withdrawal:purchase_required'] = 'yes'
+	   object.tags['cash_withdrawal:purchase_minimum'] = 'no'
+	   object.tags['cash_withdrawal:limit'] = '200'
+	   
+	   themepark:insert('cash_withdrawal', {
+			       geom = cash_withdrawal_geom,
+			       tags = object.tags,
+					       }
+	   )
+
+	   return
+	   
+	end
+	
+	-- Wasgau
+	local company = 'wasgau'
+	local companywikidata = 'q2536857'
+	if ( brand == company or operator == company or brandwikidata == companywikidata or operatorwikidata == companywikidata ) then
+
+	   object.tags.cash_withdrawal = 'implicit'
+	   object.tags['cash_withdrawal:purchase_required'] = 'yes'
+	   object.tags['cash_withdrawal:purchase_minimum'] = '20'
+	   object.tags['cash_withdrawal:limit'] = '200'
+	   
+	   themepark:insert('cash_withdrawal', {
+			       geom = cash_withdrawal_geom,
+			       tags = object.tags,
+					       }
+	   )
+
+	   return
+	   
+	end
+	
+	
+	
 end)
 
 themepark:add_proc('way', function(object, data)
